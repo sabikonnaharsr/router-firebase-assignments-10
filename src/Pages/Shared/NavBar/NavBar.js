7b5787159a318cc8e58caefd5c843a71c2037d41
sabikonnahar.sr <sabikonnahar.sr@gmail.com>
2022-10-26 04:32:17 +0600

    added title

--- src/Pages/Shared/NavBar/NavBar.js
+++ src/Pages/Shared/NavBar/NavBar.js
@@ -7,17 +7,26 @@ const NavBar = () => {
     return (
       <div className="navbar bg-base-100">
   <div className="navbar-start">
+ 
     <div className="dropdown">
       <label tabIndex={0} className="btn btn-ghost btn-circle">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
       </label>
+        <NavLink className='p-4 text-2xl font-bold'>Explore Knowledge</NavLink>
+        <NavLink className='p-4'>Homepage</NavLink>
+        <NavLink className='p-4'>Courses</NavLink>
+        <NavLink className='p-4'>Blogs</NavLink>
+      
       <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
         <li><a>Homepage</a></li>
-        <li><a>Portfolio</a></li>
-        <li><a>About</a></li>
+        <li><a>Courses</a></li>
+        <li><a>Blogs</a></li>
       </ul>
     </div>
   </div>
+  <div>
+  </div>
+  <div/>
   <div className="navbar-center">
     <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
   </div>
