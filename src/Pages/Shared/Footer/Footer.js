2e56afc18dc0702c2094626334a8134707c1aa8f
sabikonnahar.sr <sabikonnahar.sr@gmail.com>
2022-10-26 05:12:02 +0600

    added icon

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaBeer, FaFacebook, FaLinkedin, FaYoutube } from 'react-icons/fa';
 
 const Footer = () => {
     return (
@@ -18,27 +19,25 @@ const Footer = () => {
       <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a> 
       <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
     </div> */}
-    <Link >
-    <p class="fs-2 mx-3">
-          <i class="fa-brands fa-facebook"></i>
+    <Link class="fs-2 mx-3 flex justify-center items-center">
+    <p >
+      <FaFacebook> <Link href="https://twitter.com/mahima30333398?t=dgiXUV1V4Ksf0qs53MU1cQ&s=07& fbclid=IwAR3LhhG2EvEPK8Xoe_NC0MFxhRu3zpCF01ZpqjTLPqmf5q5HAC-naPh5Z9g"></Link>
+      </FaFacebook>  
+    </p>
+     <p >
+        <FaYoutube>
+        <Link href="https://youtu.be/Zv1c1ldMy6g?list=RDZv1c1ldMy6g">
+            <i className ="fa-brands text-3xl fa-square-youtube"></i>
+          </Link>
+        </FaYoutube>
         </p>
+        <FaLinkedin>
         <p class="fs-2 mx-3">
-          <a href="https://twitter.com/mahima30333398?t=dgiXUV1V4Ksf0qs53MU1cQ&s=07&fbclid=IwAR3LhhG2EvEPK8Xoe_NC0MFxhRu3zpCF01ZpqjTLPqmf5q5HAC-naPh5Z9g">
-          <i class="fa-brands fa-twitter"></i>
-        </a>
-        </p>
-
-        <p class="fs-2 mx-3">
-          <a href="https://www.linkedin.com/signup/cold-join?session_redirect=https%3A%2F%2Fwww%2Elinkedin%2Ecom%2Fpublic-profile%2Fsettings%3Ffbclid%3DIwAR0VfLffViXsqfcrMohp0dxrU38xmk3V7tFaEUohnc7-f-ZpXxSURrhNEkM&trk=login_reg_redirect">
+          <Link href="https://www.linkedin.com/signup/cold-join?session_redirect=https%3A%2F%2Fwww%2Elinkedin%2Ecom%2Fpublic-profile%2Fsettings%3Ffbclid%3DIwAR0VfLffViXsqfcrMohp0dxrU38xmk3V7tFaEUohnc7-f-ZpXxSURrhNEkM&trk=login_reg_redirect">
             <i class="fa-brands fa-linkedin"></i>
-          </a>
+          </Link>
          </p>
-
-        <p class="fs-2 mx-3">
-          <a href="https://youtu.be/Zv1c1ldMy6g?list=RDZv1c1ldMy6g">
-            <i class="fa-brands fa-square-youtube"></i>
-          </a>
-        </p>
+        </FaLinkedin>
     </Link>
   </div>
 </footer>
